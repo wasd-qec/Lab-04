5602cbf0b2d357e48855b42d6a63e4a63e73b849
kolboth <rithchankolboth@gmail.com>
2025-11-25 08:13:01 +0700

    finishex5

--- lab4ex5.java
+++ lab4ex5.java
@@ -2,24 +2,32 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 public class lab4ex5 {
    public static void main(String[] var0) throws IOException {
+
       File myfile = new File("Ex3StudentData.csv");
       Scanner sc = new Scanner(myfile);
       List<String> student = new ArrayList<>();
       
-      while (sc.hasNextLine()) {
-            student.add(sc.nextLine());
+      while (sc.hasNext()) {
+            student.add(sc.nextLine()+"\n");
         }
       sc.close();  
+      System.out.print("file:");
+      for (String s : student){
+            System.out.print(student);
+      }
       FileWriter fw = new FileWriter(myfile);
+      
       System.out.print("Number of student you want to add:");
       Scanner sc2 = new Scanner(System.in);
       int n = sc2.nextInt();
       sc2.nextLine();
+
       for (int i=0; i<n; i++){
          System.out.println("This is student: "+ (i+1));
          System.out.print("Enter student name:");
@@ -28,9 +36,23 @@ public class lab4ex5 {
          String ID = sc2.nextLine();
          System.out.print("Enter Gender: ");
          String gender = sc2.nextLine(); 
-         fw.write(name+",");
-         fw.write(ID+",");
-         fw.write(gender+"\n");
+         System.out.print("\n\n");
+         student.add(name+","+ID+","+gender+"\n");
+      }
+      System.out.println("file+write");
+      for (String s : student){
+            System.out.println(s);
+    }
+      Collections.sort(student); 
+         // {
+         //    @Override
+         //    public int compare(String s1, String s2) {
+         //        return Character.compare(s1.charAt(0), s2.charAt(0));
+         //    }
+         // });
+      System.out.println("Sorted");
+      for (String s : student){
+            System.out.println(s);
       }
       for (String s : student){
          fw.write(s);
